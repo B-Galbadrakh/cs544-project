10be2ba635be0aacfaa21b0bcf9147e5b627f2dd
Mark Ghatas <marksamyhanna@gmail.com>
2020-05-13 16:02:25 -0500

    Adding success testcases

--- src/test/java/cs544/group1/project/testcases/UserTestCases.java
+++ src/test/java/cs544/group1/project/testcases/UserTestCases.java
@@ -9,10 +9,14 @@ import cs544.group1.project.service.UserService;
 import cs544.group1.project.service.impl.UserServiceImpl;
 import cs544.group1.project.util.CustomError;
 import cs544.group1.project.util.CustomObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.MockitoJUnit;
+import org.mockito.junit.MockitoRule;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.mail.SimpleMailMessage;
@@ -22,82 +26,115 @@ import org.springframework.util.Assert;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.ArgumentMatchers.isA;
+import static org.mockito.Mockito.*;
 
 @SpringBootTest
 public class UserTestCases {
-	
-	@Autowired
-	private UserService userService;
-	
-	@Autowired
-	private JavaMailSender javaMailSender;
 
-	@InjectMocks
-	private UserServiceImpl mockUsrService;
+    @Autowired
+    private UserService userService;
 
-	@Mock
-	private UserRepository mockedUserRepository;
+    @Autowired
+    private JavaMailSender javaMailSender;
 
-	@Mock
-	private PasswordEncoder mockedPasswordEncoder;
+    @InjectMocks
+    private UserServiceImpl mockUsrService;
 
-	@Mock
-	private CustomObjectMapper mockObjectMapper;
+    @Mock
+    private UserRepository mockedUserRepository;
 
-	@Test
-	void contextLoads() {
-		User user = new User();
-		user.setFirstName("TestUser");
-		user.setLastName("Test Lastname");
-		System.out.println(userService);
-		//assertNotNull(userService.save(user));
-	}
+    @Mock
+    private PasswordEncoder mockedPasswordEncoder;
 
-	@Test
-	void mailSender() {
-	   User user = new User();
-	   user.setFirstName("TestUser");
-	   user.setLastName("Test Lastname");
-	   System.out.println(userService);
-	   SimpleMailMessage msg = new SimpleMailMessage();
-	   msg.setTo("tmcheckersystem@gmail.com", "tmcheckersystem@gmail.com");
-	   msg.setSubject("Testing from Spring Boot");
-	   msg.setText("Hello World \n Spring Boot Email");
-	   javaMailSender.send(msg);
-	}
+    @Mock
+    private CustomObjectMapper mockObjectMapper;
 
-	@Test
-	public void testUserCreation() {
-		List<User> tempUsers = new ArrayList<>();
-		tempUsers.add(new User());
-		Mockito.when(mockedUserRepository.findByEmail(isA(String.class))).thenReturn(tempUsers);
-		try
-		{
-			userService.save(getSampleDTO());
-			Assert.isTrue(false,"email validation failed");
-		}
-		catch(CustomError exc)
-		{
-			Assert.isTrue(exc.getCode().equals(400) && exc.getMessage().contains("Email already used by another user"),"email validation successful");
-		}
-		catch(Exception exc)
-		{
-			Assert.isTrue(false,"email validation failed");
-		}
-	}
+    @BeforeEach
+    public void setup() {
+        MockitoAnnotations.initMocks(this);
+    }
 
-	public UserDTO getSampleDTO()
-	{
-		UserDTO userDTO = new UserDTO();
-		userDTO.setEmail("d@gm.cm");
-		userDTO.setFirstName("Muyinza");
-		userDTO.setLastName("Daniel");
-		userDTO.setPassword("12345");
-		userDTO.setRoles(new UserRoles[]{UserRoles.ADMIN});
-		userDTO.setGender('M');
-		return userDTO;
-	}
+    @Test
+    void contextLoads() {
+        User user = new User();
+        user.setFirstName("TestUser");
+        user.setLastName("Test Lastname");
+        System.out.println(userService);
+    }
+
+    @Test
+    void mailSender() {
+        User user = new User();
+        user.setFirstName("TestUser");
+        user.setLastName("Test Lastname");
+        System.out.println(userService);
+        SimpleMailMessage msg = new SimpleMailMessage();
+        msg.setTo("tmcheckersystem@gmail.com", "tmcheckersystem@gmail.com");
+        msg.setSubject("Testing from Spring Boot");
+        msg.setText("Hello World \n Spring Boot Email");
+        javaMailSender.send(msg);
+    }
+
+    @Test
+    public void testUserCreationException() {
+        List<User> tempUsers = new ArrayList<>();
+        tempUsers.add(new User());
+        Mockito.when(mockedUserRepository.findByEmail(isA(String.class))).thenReturn(tempUsers);
+        try {
+            userService.save(getSampleDTO());
+            Assert.isTrue(false, "email validation failed");
+        } catch (CustomError exc) {
+            Assert.isTrue(exc.getCode().equals(400) && exc.getMessage().contains("Email already used by another user"), "email validation successful");
+        } catch (Exception exc) {
+            Assert.isTrue(false, "email validation failed");
+        }
+    }
+
+    @Test
+    public void testUserCreationSuccess() throws CustomError {
+        UserDTO userDTO = getNewUserDTO();
+        User user = userService.save(userDTO);
+        assertNotNull(user);
+    }
+
+    public UserDTO getSampleDTO() {
+        UserDTO userDTO = new UserDTO();
+        userDTO.setEmail("d@gm.cm");
+        userDTO.setFirstName("Mark");
+        userDTO.setLastName("Daniel");
+        userDTO.setPassword("12345");
+        userDTO.setRoles(new UserRoles[]{UserRoles.ADMIN});
+        userDTO.setGender('M');
+        return userDTO;
+    }
+
+    public UserDTO getNewUserDTO() {
+        UserDTO userDTO = new UserDTO();
+        userDTO.setEmail(generateNewEmail());
+        userDTO.setFirstName("Mark");
+        userDTO.setLastName("Ghatas");
+        userDTO.setPassword("3124124");
+        userDTO.setRoles(new UserRoles[]{UserRoles.ADMIN});
+        userDTO.setGender('M');
+        return userDTO;
+    }
+
+    String generateNewEmail() {
+        String characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+        StringBuilder email = new StringBuilder();
+        Random rnd = new Random();
+        while (email.length() < 10) {
+            int index = (int) (rnd.nextFloat() * characters.length());
+            email.append(characters.charAt(index));
+        }
+        String res = email.toString() + "@gmail.com";
+
+        return res;
+    }
 
 }
