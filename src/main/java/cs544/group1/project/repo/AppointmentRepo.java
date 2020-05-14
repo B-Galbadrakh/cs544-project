a301d17e2043d20fdbd1b25b3afda446d77c2293
Ashenafi <ashenafi66@gmail.com>
2020-05-13 23:51:11 -0500

    transaction added for repos'

--- src/main/java/cs544/group1/project/repo/AppointmentRepo.java
+++ src/main/java/cs544/group1/project/repo/AppointmentRepo.java
@@ -3,8 +3,10 @@ package cs544.group1.project.repo;
 import cs544.group1.project.domain.Appointment;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 @Repository
+@Transactional
 public interface AppointmentRepo extends JpaRepository<Appointment, Integer>{
 
 }
--- src/main/java/cs544/group1/project/repo/UserRepository.java
+++ src/main/java/cs544/group1/project/repo/UserRepository.java
@@ -5,10 +5,12 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import cs544.group1.project.domain.User;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Repository
+@Transactional
 public interface UserRepository extends JpaRepository<User, Integer> {
     @Query(value = "SELECT u from User u left join fetch u.role where u.email = :email")
     List<User> findByEmail(String email);
